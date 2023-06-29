eff3f2e6612960943e53a3066f90a68663a4d3ab
Thiago Bourscheid <thiagobourscheid@gmail.com>
2023-06-29 22:13:47 +0100

    Saving to Persistent Path

--- ModelViewer/Assets/Scripts/ModelFetcher.cs
+++ ModelViewer/Assets/Scripts/ModelFetcher.cs
@@ -4,27 +4,39 @@ using System;
 
 using UnityEngine.Networking;
 using System.IO;
+using Siccity.GLTFUtility;
 
 public class ModelFetcher : MonoBehaviour {
+    // Config
+    [SerializeField] float modelScale = 0.03f;
 
-    // this is a temp method as in the future Download3DModel should be called by the teams client 
-    void Update() {
-        if (Input.GetKeyDown("t")) {
-            Download3DModel();
-        }
+    // Params
+    String fileName = "test.glb";
+    String persistentPath;
+
+
+    // Cache
+    ModelRotationController controller;
+
+    public void Start() {
+        controller = GetComponent<ModelRotationController>();
+        persistentPath = Application.persistentDataPath;
+        Debug.Log(persistentPath);
     }
 
     //public void Download3DModel(string hid)
     public void Download3DModel() {
         //string url = $"https://organsegmentation-storageaccessor-app.azurewebsites.net/api/v1/holograms/{hid}/download";
+        //fileName = hid
         // current hardcoded url    
         string url = "https://organsegmentation-storageaccessor-app.azurewebsites.net/api/v1/holograms/04bcb170b8c9846c1830a29b4b465bbd/download";
-        string fileName = "test.glb";
-
-        StartCoroutine(DownloadFile(url, fileName));
+   
+        StartCoroutine(DownloadFile(url));
     }
 
-    private IEnumerator DownloadFile(string url, string filePath) {
+    private IEnumerator DownloadFile(string url) {
+        String fullPath = Path.Combine(persistentPath, fileName);
+
         UnityWebRequest webRequest = UnityWebRequest.Get(url);
 
         yield return webRequest.SendWebRequest();
@@ -32,18 +44,15 @@ public class ModelFetcher : MonoBehaviour {
         if (webRequest.result == UnityWebRequest.Result.Success) {
             byte[] content = webRequest.downloadHandler.data;
 
-            string persistentPath = "./Assets/Resources/Models/" + filePath;
-            File.WriteAllBytes(persistentPath, content);
+            File.WriteAllBytes(fullPath, content);
 
             Debug.Log("File downloaded successfully.");
 
-            //LoadModel(persistentPath);
-
             // Display the file path and file size
-            FileInfo fileInfo = new FileInfo(persistentPath);
+            FileInfo fileInfo = new FileInfo(fullPath);
             Debug.Log($"File path: {fileInfo.FullName}");
             Debug.Log($"File size: {fileInfo.Length} bytes");
-            ConvertGLBToString(persistentPath); // for debugging purposes, should be removed in the future  
+            ConvertGLBToString(fullPath); // for debugging purposes, should be removed in the future  
         } else {
             Debug.Log($"Failed to download file. Error: {webRequest.error}");
         }
@@ -57,6 +66,43 @@ public class ModelFetcher : MonoBehaviour {
             Debug.Log("GLB length as Base64 string:\n" + glbString.Length.ToString());
         } else {
             Debug.LogError("GLB file not found: " + filePath);
+        }
+    }
+
+    public void CreateObject() {
+        String fullPath = Path.Combine(persistentPath, fileName);
+
+        // Load the GLB file from the Resources folder
+        GameObject targetModel = Importer.LoadFromFile(fullPath);
+
+        // Check if the Model was loaded successfully
+        if (targetModel == null) {
+            Debug.LogError("Failed to load object from Resources folder.");
+            return;
+        }
+
+
+
+        // Destroy Current Target Model
+        Destroy(GameObject.Find("Target Model"));
+        controller.target = targetModel;
+
+        // Set Model Properties
+        targetModel.name = "Target Model";
+        targetModel.transform.localScale *= modelScale;
+
+
+        // Set this as Parent of Model
+        targetModel.transform.parent = transform;
+    }
+
+    // this is a temp method as in the future Download3DModel should be called by the teams client 
+    void Update() {
+        if (Input.GetKeyDown("t")) {
+            Download3DModel();
+        }
+        if (Input.GetKeyDown("y")) {
+            CreateObject();
         }
     }
 }
