cb4f2daf5bb76393071dde2bc0acd4b97720ebba
Stanley-deng <74106358+Stanley-deng@users.noreply.github.com>
2023-07-01 17:57:01 +0100

    update package to use UnityEngine.Networking

--- ModelViewer/Assets/OBJImport/Samples/ObjFromStream.cs
+++ ModelViewer/Assets/OBJImport/Samples/ObjFromStream.cs
@@ -2,16 +2,28 @@
 using System.IO;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
 
-public class ObjFromStream : MonoBehaviour {
-	void Start () {
-        //make www
-        var www = new WWW("https://people.sc.fsu.edu/~jburkardt/data/obj/lamp.obj");
-        while (!www.isDone)
-            System.Threading.Thread.Sleep(1);
-        
-        //create stream and load
-        var textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.text));
-        var loadedObj = new OBJLoader().Load(textStream);
-	}
+public class ObjFromStream : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        string url = "https://people.sc.fsu.edu/~jburkardt/data/obj/lamp.obj";
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Error downloading OBJ file: " + webRequest.error);
+                yield break;
+            }
+
+            // Create stream and load
+            var textStream = new MemoryStream(Encoding.UTF8.GetBytes(webRequest.downloadHandler.text));
+            var loadedObj = new OBJLoader().Load(textStream);
+        }
+    }
 }
