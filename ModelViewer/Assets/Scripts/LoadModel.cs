c9d8cae83a2e633bebf957212d78af0b4f8b8b72
Stanley-deng <74106358+Stanley-deng@users.noreply.github.com>
2023-07-06 13:49:27 +0100

    add two version of the progress bar for loading

---
+++ ModelViewer/Assets/Scripts/LoadModel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadModel : MonoBehaviour
+{
+    //[SerializeField] public Image ProgressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- ModelViewer/Assets/Scripts/ModelRotationController.cs
+++ ModelViewer/Assets/Scripts/ModelRotationController.cs
@@ -86,9 +86,9 @@ public class ModelRotationController : MonoBehaviour {
         Vector3 r = Transform.localEulerAngles;
         float[] e = new float[] { r.x, r.y, r.z };
 
-        try {
-            SyncRotation(r.x, r.y, r.z);
-        } catch { }
+        //try {
+            //SyncRotation(r.x, r.y, r.z);
+        //} catch { }
     }
 
 
