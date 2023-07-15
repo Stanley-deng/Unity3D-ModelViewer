2617486a8590e4f6c8715a2c31ac650045850861
Thiago Bourscheid <thiagobourscheid@gmail.com>
2023-07-15 20:53:48 +0100

    Improved Model Scaling for Imported Models

--- ModelViewer/Assets/Scripts/ModelScaleController.cs
+++ ModelViewer/Assets/Scripts/ModelScaleController.cs
@@ -8,13 +8,13 @@ public class ModelScaleController : MonoBehaviour
     // Config
     // The speed of scaler when using keys
     [SerializeField] float speed = 5f;
-    // The speed of scaler when using multi-touch
     // Default scale
     [SerializeField] Vector3 defaultScale = new Vector3(250f, 250f, 250f);
-
     // Get the transform of target object
-    public GameObject target;
-    Transform Transform { get { return target.transform; } }
+    [SerializeField] GameObject target;
+    Transform Transform { get { return Target.transform; } }
+
+    public GameObject Target { get => target; set { target = value; defaultScale = target.transform.lossyScale; } }
 
     void ResetScale()
     {
@@ -36,8 +36,15 @@ public class ModelScaleController : MonoBehaviour
         }
     }
 
+    void ChangeScaleRelative(float delta) {
+        float percentageChange = delta / 100;
+        delta = 1 + percentageChange;
+        Transform.localScale = Vector3.Scale(Transform.localScale, new Vector3(delta, delta, delta));
+    }
+
+
     void OnGUI() {
-        ChangeScale(speed * Input.mouseScrollDelta.y);
+        ChangeScaleRelative(speed * Input.mouseScrollDelta.y);
     }
 }
 
